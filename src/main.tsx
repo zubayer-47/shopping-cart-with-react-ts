8304eb4aba25418230a5d5ce3557269530be387a
A B M Zubayer <zubayerabm47@gmail.com>
2023-09-30 13:19:02 +0600

    migrate from create-react-app to vite

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { store } from './app/store';
+import ErrorBoundary from './components/errors/ErrorBoundary';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<React.StrictMode>
+		<ErrorBoundary>
+			<Provider store={store}>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</Provider>
+		</ErrorBoundary>
+	</React.StrictMode>
+);
